57f784973c76c495e7633678145397e967c164fb
mfao <medieninformatik@markusfolz.com>
2020-12-22 15:10:26 +0100

    Tutorial finished

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
-import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
+import Layout, { siteTitle } from "../components/layout";
+import Date from "../components/date";
 
-export default function Home() {
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Home({ allPostsData }) {
 	return (
 		<Layout home>
 			<Head>
@@ -14,9 +25,23 @@ export default function Home() {
 					Hi, I'm Markus. I may not be a stereotype programmer (not
 					the biggest nerd, lol), but I love me some good code!
 				</p>
-				<Link href="/posts/first-post">
-					<a>Check out my first Post</a>
-				</Link>
+			</section>
+			<section
+				className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							<Link href={`/posts/${id}`}>
+								<a>{title}</a>
+							</Link>
+							<br />
+							<small className={utilStyles.lightText}>
+								<Date dateString={date} />
+							</small>
+						</li>
+					))}
+				</ul>
 			</section>
 		</Layout>
 	);
